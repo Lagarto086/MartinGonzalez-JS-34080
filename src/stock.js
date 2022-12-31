0e1e835ba4ac423ae1074ee6db665588f52fff94
Martin Fabian Gonzalez Castelo <mfgc67@gmail.com>
2022-12-30 23:45:24 -0300

    Entrega Final

---
+++ src/stock.js
@@ -0,0 +1,37 @@
+const productos =
+[
+    {
+        id: 101,
+        nombre: "Camiseta Oficial",
+        tipo: "Deportiva",
+        desc: "Camiseta oficial del CNF, Temporada 2023",
+        precio: 4500,
+        talle: "L",
+        img: './public/images/camiseta.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 102,
+        nombre: "Canguro",
+        tipo: "Abrigo",
+        desc: "Canguro con logo del CNF",
+        precio: 3800,
+        talle: "L",
+        img: './public/images/canguro.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 103,
+        nombre: "Short Oficial",
+        tipo: "Deportiva",
+        desc: "Short oficial del CNF, Temporada 2023",
+        precio: 900,
+        talle: "S",
+        img: './public/images/short.jpg',
+        cantidad:1
+    },
+]
+
+export { productos };
